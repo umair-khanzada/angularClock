984b45819f3b7ec5e8439e3e2410b6d1f2d01b98
Umair Zulfiqar <umairkz52@gmail.com>
2017-01-14 15:52:10 +0500

    refactor code.

--- app.js
+++ app.js
@@ -5,24 +5,36 @@
 angular.module('angularClock', [])
     .directive('clock', function( $interval ) {
         'ngInject';
+
         return{
             restrict: 'E',
             scope: {
-                    color: '@?', //color code #ffffff or color name white.
-                    bgColor: '@?', //color code #ffffff or color name white.
-                    hourNumber: '<?', //hour-number Boolean show or hide.
-                    size: '<?' //size of the clock.
+                    color: '@?',        //color code #ffffff or color name white.
+                    bgColor: '@?',      //color code #ffffff or color name white.
+                    hourNumber: '<?',   //hour-number Boolean show or hide.
+                    size: '@?'          //size of the clock.
             },
             templateUrl: 'clock-template.html',
             link: function(scope){
+
+                //Declaration.
+                var clock, now, sec, min, hour, secAngle, minAngle, hourAngle, secHand, minHand, hourHand;
+
                 //set default values of component.
                 scope.color = scope.color || '#000';
                 scope.bgColor = scope.bgColor || '#fff';
                 scope.hourNumber = scope.hourNumber || false;
                 scope.size = scope.size || 100;
 
-                //Declaration.
-                var now, sec, min, hour, secAngle, minAngle, hourAngle, secHand, minHand, hourHand;
+                //set width and height of clock.
+                clock = document.getElementById('clock');
+                clock.setAttribute("height", scope.size);
+                clock.setAttribute("width", scope.size);
+
+                //get clock handles.
+                secHand = document.getElementById('second-hand');
+                minHand = document.getElementById('minute-hand');
+                hourHand = document.getElementById('hour-hand');
 
                 function startClock(){
                     now = new Date();
@@ -32,11 +44,6 @@ angular.module('angularClock', [])
                     secAngle = sec*6;
                     minAngle = min*6;
                     hourAngle = hour*30;
-
-                    //get clock handles.
-                    secHand = document.getElementById('second-hand');
-                    minHand = document.getElementById('minute-hand');
-                    hourHand = document.getElementById('hour-hand');
 
                     //rotate clock handles.
                     secHand.setAttribute("transform", "rotate(" + secAngle + ",50,50)");
